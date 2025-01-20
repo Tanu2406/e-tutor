942d9a8bba45ce8b438764c1bb99d3cd73b92377
Tanuja Shekokar <tanu.shekokar@gmail.com>
2025-01-20 11:34:54 +0530

    vite.config.js file modify

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Default output directory for Vite builds
+  },
 })
-// Vite: A modern build tool that provides fast development servers and builds.
-// React Plugin: Integrates React features like JSX transformation with Vite.
-
-// defineConfig: Defines the configuration for the Vite project.
-// Plugins:
-// The react() plugin adds React-specific functionalities, like support for JSX, HMR (Hot Module Replacement), and other optimizations.
